b6f1eff55d503bea3c3fc3deb5622103c76c78ba
AlverdeMiranda <167699396+AlverdeMiranda@users.noreply.github.com>
2024-08-29 17:48:12 -0300

    Add files via upload

---
+++ tp1_pmiw/tp1_pmiw.js
@@ -0,0 +1,45 @@
+let cant; modX; modY;
+function preload(){
+  obratp3 = loadImage("data/obratp3.jpg");
+}
+function setup() {
+createCanvas(800,400);
+cant = 9;
+modX = 400/cant;
+modY = 400/cant;
+for (let j = 0; j < cant; j++) {
+    for (let i = 0; i < cant; i++) {
+      if ((i+j)%2==0) {
+       color(0);
+      } else {
+       color(255);
+      }
+    }
+  }
+}
+function draw() {
+   background(255);
+  image(obratp3,0,0,400,400);
+   for (let j = 0; j < cant; j++) {
+    for (let i = 0; i < cant; i++) {
+      if((i+j)%2==0){
+      fill(0);
+      }
+      rect(400 + i * modX, j * modY, modX, modY);
+      if ((i + j) % 2 == 0) {
+        let cuadChicos = modX / 5;
+        fill(255);
+        for (let m = 0; m < 2; m++) {
+          for (let n = 0; n < 2; n++) {
+            let cuadX = 400 + i * modX + m * (modX - cuadChicos);
+            let cuadY = j * modY + n * (modY - cuadChicos);
+            rect(cuadX, cuadY, cuadChicos, cuadChicos);
+          }
+        }
+      }
+    }
+  }
+}
+function keyPressed() {
+  
+}
